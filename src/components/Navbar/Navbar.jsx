19abd5add641295741986c2cf431a099a91a968a
neelarani <neela9624@gmail.com>
2025-04-22 20:19:21 +0600

    changes font size btns colors

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -119,7 +119,7 @@ const Navbar = () => {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-black text-white rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-black text-white rounded-box z-[1] mt-3 w-52 p-2 shadow font-semibold"
             >
               {links}
             </ul>
@@ -133,10 +133,10 @@ const Navbar = () => {
             />
           </Link>
 
-          <a className=" text-base md:text-xl">Service Sharing</a>
+          <a className=" text-base md:text-xl font-bold">Service Sharing</a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">{links}</ul>
+          <ul className="menu menu-horizontal px-1 font-semibold">{links}</ul>
         </div>
 
         <div className="navbar-end ">
@@ -164,7 +164,7 @@ const Navbar = () => {
               </button>
             </div>
           ) : (
-            <div className="space-x-2">
+            <div className="space-x-2 font-semibold">
               <Link to={'/login'}>Login</Link>
               <Link to={'/register'}>Register</Link>
             </div>
